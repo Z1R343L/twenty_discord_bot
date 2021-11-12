da452161098ff568cc2b629f7a1edbaeb0804416
Z1R343L <jan2705@gmail.com>
2021-11-12 12:59:41 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.10-bullseye
 COPY . .
-RUN pip install -q -r requirements.txt > /dev/null 2>&1
+RUN pip install -q -U setuptools wheel > /dev/null 2>&1
+RUN pip install -q -r requirements.txt
 CMD ["python3", "-u", "main.py"]
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import logging
 from os import getenv, name
 from urllib.parse import urlencode
 from base64 import urlsafe_b64encode, urlsafe_b64decode
-import pickle
+from time import time
+import srsly
 
 import disnake
 import uvloop
@@ -35,8 +36,20 @@ async def fetch_endpoint(url: str, param: dict = {}) -> dict:
             return await response.json()
 
 async def message_hook(message, ID: str) -> None:
-    new_data = pickle.dumps({"channel_id": message.channel.id, "message_id": message.id}, protocol=pickle.HIGHEST_PROTOCOL)
-    await fetch_endpoint(url=f"{baseurl}set?", param={"ID": ID, "prefix": "message", "data": urlsafe_b64encode(new_data)})
+    new_data = srsly.msgpack_dumps(
+        {
+            "channel_id": message.channel.id,
+            "message_id": message.id
+        }
+    )
+    await fetch_endpoint(
+        url=f"{baseurl}set?",
+        param={
+            "ID": ID,
+            "prefix": "message",
+            "data": urlsafe_b64encode(new_data)
+        }
+    )
  
 
 
@@ -47,8 +60,12 @@ async def play_view(possible_moves: dict, score: int, name: str) -> disnake.ui.V
         for btn in row:
             if btn.startswith("none_"):
                 view.add_item(disnake.ui.Button(
-                    emoji=emojis["none"], custom_id=btn, disabled=True, row=row_index
-                ))
+                    emoji=emojis["none"],
+                    custom_id=btn,
+                    disabled=True,
+                    row=row_index
+                )
+            )
             elif btn == "exit":
                 view.add_item(
                     disnake.ui.Button(
@@ -75,9 +92,13 @@ class continue_select(disnake.ui.Select):
     def __init__(self):
         options = [
             disnake.SelectOption(
-                label="yes", description="yes => continue running game!"
+                label="yes",
+                description="yes => continue running game!"
             ),
-            disnake.SelectOption(label="no", description="no => start new game!"),
+            disnake.SelectOption(
+                label="no",
+                description="no => start new game!"
+            )
         ]
         super().__init__(
             placeholder="found running game, continue?",
@@ -88,16 +109,32 @@ class continue_select(disnake.ui.Select):
         )
 
     async def callback(self, inter: disnake.MessageInteraction):
-        await inter.response.defer()
         match self.values[0]:
             case 'no':
                 url = f"{baseurl}new_game?"
+                data = await fetch_endpoint(
+                    url=url,
+                    param={
+                        "ID": inter.author.id,
+                        "name": inter.author.name
+                    }
+                )
+                await inter.response.defer()
+                await inter.message.edit(
+                    file=disnake.File(data['image_path']),
+                    attachments=[],
+                    view=await play_view(
+                        possible_moves=data["possible_moves"],
+                        score=data['score'],
+                        name=inter.author.name
+                    )
+                )
             case 'yes':
                 url = f"{baseurl}data?"
-        data = await fetch_endpoint(url=url, param={"ID": inter.author.id, "name": inter.author.name})
-        await inter.message.edit(file=disnake.File(data['image_path']), attachments=[], view=await play_view(possible_moves=data["possible_moves"], score=data['score'], name=inter.author.name))
-
-
+                data = await fetch_endpoint(url=url, param={"ID": inter.author.id, "name": inter.author.name})
+                await inter.response.defer()
+                await inter.message.edit(view=await play_view(possible_moves=data["possible_moves"], score=data['score'], name=inter.author.name))
+        
 class continue_select_view(disnake.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
@@ -115,7 +152,11 @@ class continue_select_view(disnake.ui.View):
 
 class Bot(commands.AutoShardedBot):
     def __init__(self):
-        super().__init__(command_prefix="_", asyncio_debug=True)
+        super().__init__(
+            command_prefix='_',
+            asyncio_debug=True,
+            sync_commands_debug=True
+        )
         self.persistent_views_added = False
 
     async def on_ready(self):
@@ -127,29 +168,66 @@ bot = Bot()
 
 @bot.slash_command()
 async def play(inter: disnake.ApplicationCommandInteraction) -> None:
-    data = await fetch_endpoint(url=f"{baseurl}data?", param={"ID": inter.author.id, "name": inter.author.name})
+    data = await fetch_endpoint(
+        url=f"{baseurl}data?",
+        param={
+            "ID": inter.author.id, 
+            "name": inter.author.name
+        }
+    )
     if data["can_continue"] == 1:
         view = continue_select_view()
     else:
-        view = await play_view(possible_moves=data["possible_moves"], score=data['score'], name=inter.author.name)
-    msg = await inter.response.send_message(file=disnake.File(data['image_path']), view=view)
-    await message_hook(message=await inter.original_message(), ID=str(inter.author.id))
+        view = await play_view(
+            possible_moves=data["possible_moves"],
+            score=data['score'],
+            name=inter.author.name
+        )
+    await inter.response.send_message(
+        file=disnake.File(data['image_path']),
+        view=view
+    )
+    await message_hook(
+        message=await inter.original_message(),
+        ID=str(inter.author.id)
+    )
 
 
 @bot.event
 async def on_button_click(inter) -> None:
-    await inter.response.defer()
-    raw = await fetch_endpoint(url=f"{baseurl}get?", param={"ID": inter.author.id, "name": inter.author.name, "prefix": "message"})
+    raw = await fetch_endpoint(
+        url=f"{baseurl}get?",
+        param={
+            "ID": inter.author.id,
+            "name": inter.author.name,
+            "prefix": "message"
+        }
+    )
     if raw['success']:
-        data = pickle.loads(urlsafe_b64decode(raw['data']))
-        msg = await inter.original_message()
+        await inter.response.defer()
+        data = srsly.msgpack_loads(urlsafe_b64decode(raw['data']))
+        msg = inter.message
         if data['message_id'] == msg.id:
             if inter.component.custom_id == "exit":
-                await inter.response.defer()
                 await inter.message.delete()
                 return
-            data = await fetch_endpoint(url=f"{baseurl}move?", param={"ID": inter.author.id, "action": inter.component.custom_id})
+            data = await fetch_endpoint(
+                url=f"{baseurl}move?",
+                param={
+                    "ID": inter.author.id,
+                    "action": inter.component.custom_id
+                }
+            )
             c = 'Game Over!' if data["possible_moves"]['over'] else ''
-            await inter.edit_original_message(content=c, file=disnake.File(data['image_path']), attachments=[], view=await play_view(possible_moves=data["possible_moves"], score=data['score'], name=inter.author.name))
+            await inter.edit_original_message(
+                content=c,
+                file=disnake.File(data['image_path']),
+                attachments=[],
+                view=await play_view(
+                    possible_moves=data["possible_moves"],
+                    score=data['score'],
+                    name=inter.author.name
+                )
+            )
 
 bot.run(getenv("DISCORD_TOKEN"))
